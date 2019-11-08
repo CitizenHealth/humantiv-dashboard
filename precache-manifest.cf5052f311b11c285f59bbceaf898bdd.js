130fe85a25386d9e8436797e5639930b561e8d3a
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2019-11-08 00:52:27 -0800

    Updates

---
+++ precache-manifest.cf5052f311b11c285f59bbceaf898bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792919f9d450fc6356ffa18568c0dd15",
+    "url": "/CitizenHealth/humantiv-dashboard/index.html"
+  },
+  {
+    "revision": "37bf93f037e990331833",
+    "url": "/CitizenHealth/humantiv-dashboard/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "10a9943518794cc4894e",
+    "url": "/CitizenHealth/humantiv-dashboard/static/js/2.5d7a1087.chunk.js"
+  },
+  {
+    "revision": "37bf93f037e990331833",
+    "url": "/CitizenHealth/humantiv-dashboard/static/js/main.bdfac48f.chunk.js"
+  },
+  {
+    "revision": "73a81a061954a0e08de5",
+    "url": "/CitizenHealth/humantiv-dashboard/static/js/runtime-main.dfdc58ff.js"
+  },
+  {
+    "revision": "da3e88f73cd39967210e99dea603cad3",
+    "url": "/CitizenHealth/humantiv-dashboard/static/media/logo.da3e88f7.png"
+  }
+]);
